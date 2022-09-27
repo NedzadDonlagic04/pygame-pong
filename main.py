2c7056d82c5e45da0a30e80b79c3045bafa46e88
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-27 12:46:03 +0200

    Added Game class
    The class will be used to initialize the game,
    run the game and exit the game.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import pygame as py
+import pygame
+from sys import exit
 
-def main():
-    print('Hello world!')
+class Game:
+    def __init__(self, width, height, title):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((width, height))
+
+        pygame.display.set_caption(title)
+
+    def closeGame(self):
+        pygame.quit()
+        exit()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.closeGame()
+
+            pygame.display.update()
 
 if __name__ == '__main__':
-    main()
+    game = Game(600, 400, 'Pong')
+    game.run()
