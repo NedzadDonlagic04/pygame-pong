8ebf11e62be6744dd9adbd2e8f641d39785a3bc1
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-28 17:54:07 +0200

    Implemented on start screen text

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ class Game:
 
         self.clock = Clock(60)
 
+        font = pygame.font.Font('./fonts/Pixeltype.ttf', 150)
+        self.image1 = font.render('Pong', False, 'White')
+        self.rect1 = self.image1.get_rect( center = (width / 2, 100))
+
+        font = pygame.font.Font('./fonts/Pixeltype.ttf', 80)
+        self.image2 = font.render('Play', False, 'Gray')
+        self.rect2 = self.image2.get_rect( center = (width / 2, self.rect1.bottom + 70))
+
+        font = pygame.font.Font('./fonts/Pixeltype.ttf', 80)
+        self.image3 = font.render('Exit', False, 'Gray')
+        self.rect3 = self.image3.get_rect( center = (width / 2, self.rect2.bottom + 50))
+
+        pongText = Text(150, 'Pong', 'White', (width / 2, 100))
+        startText = Text(80, 'Play', 'Gray', (width / 2, pongText.rect.bottom + 70), True)
+        exitText = Text(80, 'Exit', 'Gray', (width / 2, startText.rect.bottom + 50), True)
+
+        self.startScreenText = pygame.sprite.Group(pongText, startText, exitText)
+
     def closeGame(self):
         pygame.quit()
         exit()
@@ -21,6 +39,9 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.closeGame()
+
+            self.startScreenText.update()
+            self.startScreenText.draw(self.screen)
 
             pygame.display.update()
             self.clock.tick()
