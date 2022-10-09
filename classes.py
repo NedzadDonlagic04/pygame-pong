9963198e70a101c28905eac2a165a2c36775df94
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-10-09 13:38:33 +0200

    Defined ball class

--- classes.py
+++ classes.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randrange, choice
 
 class Clock:
     def __init__(self, fps):
@@ -58,3 +59,36 @@ class MouseEvent:
                 if sprite.rect.collidepoint(pos):
                     self.text = sprite.getText()
                     return
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        ball_img = pygame.Surface((15, 15), pygame.SRCALPHA)
+        pygame.draw.circle(ball_img, (255, 255, 255), (7.5, 7.5), 7.5)
+        self.image = ball_img
+
+        self.SCREEN_WIDTH = width
+        self.SCREEN_HEIGHT = height
+
+        self.directions = [-1 , 1]
+
+    def setSpot(self):
+        y = randrange(8, self.SCREEN_HEIGHT - 8, 1)
+        self.rect = self.image.get_rect( center = (self.SCREEN_WIDTH/2, y))
+        
+        self.x = choice(self.directions)
+        self.y = choice(self.directions)
+
+    def update(self):
+        self.rect.top += self.y
+        self.rect.left += self.x
+
+        if self.rect.top == 0 or self.rect.bottom == self.SCREEN_HEIGHT:
+            self.y = -self.y
+        if self.rect.left == 0 or self.rect.right == self.SCREEN_WIDTH:
+            self.x = -self.x
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
